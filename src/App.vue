3b9fc5886e7745661e0a59489ea7b0313d17eaa1
HighSo <toritoritorippy0911@gmail.com>
2021-11-24 22:59:19 +0900

    App.vueとSelect.vueの変更

--- src/App.vue
+++ src/App.vue
@@ -4,25 +4,33 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/select">Select</router-link>
       <router-link to="/result">Result</router-link>
+      <Question v-on:new="questions = $event" />
     </div>
     <router-view />
   </div>
 </template>
 
 <script>
+import Question from "src/views/Select.vue"
 export default {
   date() {
     return {
-      question: [
+      questions: [
         {
           id: 1,
-          question: "",
-          answer: "",
+          text: "質問",
+          answer1: "1",
+          answer2: "2",
+          answer3: "3",
           youkyado: 0,
         },
       ],
     }
+    components: {
+      Question
+    }
   },
+  methods: {},
 }
 </script>
 
