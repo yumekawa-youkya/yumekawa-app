de2122200c19588a0ab246edf5f8944c2ceb2dea
tkgkzyk <tkgkzyk1018@gmail.com>
2021-11-29 21:33:00 +0900

    Result.vueの修正

--- src/views/Result.vue
+++ src/views/Result.vue
@@ -2,12 +2,12 @@
   <div>
     <div>結果発表</div>
     <div>あなたの陽キャ度は...</div>
-    <div>{{ Totalyoukyado }}です！！！</div>
+    <div>{{ totalYoukyado }}です！！！</div>
     <div v-if="this.youkyado <= 2">ほとんど陰キャ！</div>
-    <div v-else-if="this.youkyado <= 4">ちょっと陽キャ！</div>
-    <div v-else-if="this.youkyado <= 6">まあまあ陽キャ！</div>
-    <div v-else-if="this.youkyado <= 8">かなりの陽キャ！</div>
-    <div v-else-if="this.youkyado <= 6">完全なる陽キャ！</div>
+    <div v-else-if="this.youkyadoCmp <= 4">ちょっと陽キャ！</div>
+    <div v-else-if="this.youkyadoCmp <= 6">まあまあ陽キャ！</div>
+    <div v-else-if="this.youkyadoCmp <= 8">かなりの陽キャ！</div>
+    <div v-else-if="this.youkyadoCmp <= 10">完全なる陽キャ！</div>
   </div>
 </template>
 
@@ -24,12 +24,12 @@ export default {
   },
   methods: {},
   computed: {
-    Totalyoukado: function () {
-      let youkyado
-      for (let i = 0; i < this.question.length; i++) {
-        youkyado += this.question.youkyado
+    totalYoukado: function () {
+      let youkyadoCmp
+      for (let i = 0; i < this.question[i].length; i++) {
+        youkyadoCmp += this.question[i].youkyado
       }
-      return youkyado
+      return youkyadoCmp
     },
   },
 }
