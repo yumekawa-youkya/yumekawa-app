89b82cf2c9b2dca3f7094137b3e4501ef3e35710
HighSo <toritoritorippy0911@gmail.com>
2021-11-29 21:28:31 +0900

    解答を保存するようにした

--- src/views/Select.vue
+++ src/views/Select.vue
@@ -2,9 +2,15 @@
   <div>
     <div v-for="(question, index) in questions" v-bind:key="index">
       <div>{{ question.text }}</div>
-      <button v-on:click="send">{{ question.answer1 }}</button>
-      <button v-on:click="send">{{ question.answer2 }}</button>
-      <button v-on:click="send">{{ question.answer3 }}</button>
+      <button v-on:click="send(question.answer1)">
+        {{ question.answer1 }}
+      </button>
+      <button v-on:click="send(question.answer2)">
+        {{ question.answer2 }}
+      </button>
+      <button v-on:click="send(question.answer3)">
+        {{ question.answer3 }}
+      </button>
     </div>
   </div>
 </template>
@@ -20,14 +26,14 @@ export default {
   data() {
     return {
       tempQuestions: this.questions,
+      answerQuestion: "",
     }
   },
   methods: {
-    // ボタンを押した時にquestionsを変更する関数
-    answer: function () {},
-    // App.vueを変更する関数
-    send() {
+    // App.vueを変更する、解答を保存する関数
+    send: function (answer) {
       this.$emit("new", this.tempQuestions)
+      this.answerQuestion = answer
     },
   },
 }
